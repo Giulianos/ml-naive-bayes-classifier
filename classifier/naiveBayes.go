0fd0334d7f9987461127b7ec590fc0001ccf7722
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2019-08-16 01:01:15 -0300

    Add dataset loading

--- classifier/naiveBayes.go
+++ classifier/naiveBayes.go
@@ -1,19 +1,19 @@
 package classifier
 
-type PDhKey struct {
+type _PDhKey struct {
 	feature      uint64
 	featureValue uint64
 	class        uint64
 }
 
-// NaiveBayes
+// NaiveBayes represents a Naive Bayes Classifier
 type NaiveBayes struct {
-	PDh map[PDhKey]float64
+	PDh map[_PDhKey]float64
 	Ph  []float64
 }
 
 func (nb NaiveBayes) conditionalPriorProbability(feature uint64, featureValue uint64, class uint64) float64 {
-	key := PDhKey{feature, featureValue, class}
+	key := _PDhKey{feature, featureValue, class}
 	return nb.PDh[key]
 }
 
@@ -22,7 +22,7 @@ func (nb NaiveBayes) classPriorProbability(class uint64) float64 {
 }
 
 func (nb NaiveBayes) posterioriProbability(features []uint64, class uint64) float64 {
-	var probability float64 = nb.classPriorProbability(class)
+	probability := nb.classPriorProbability(class)
 	for feature, featureValue := range features {
 		probability *= nb.conditionalPriorProbability(uint64(feature), featureValue, class)
 	}
@@ -34,43 +34,45 @@ func (nb NaiveBayes) posterioriProbability(features []uint64, class uint64) floa
 func (nb NaiveBayes) Predict(features []uint64) (uint64, float64) {
 	var maxArg uint64
 	var maxVal float64
-        var total float64
-	for class, _ := range nb.Ph {
+	var total float64
+	for class := range nb.Ph {
 		val := nb.posterioriProbability(features, uint64(class))
-                total += val
+		total += val
 		if val > maxVal || class == 0 {
 			maxArg = uint64(class)
 			maxVal = val
 		}
 	}
 
-	return maxArg, maxVal/total
+	return maxArg, maxVal / total
 }
 
+// Train receives the dataset and trains the classifier
 func (nb *NaiveBayes) Train(ds [][]uint64) {
-  freqTable := make(map[PDhKey]float64)
-  countTable := make(map[uint64]float64)
+	freqTable := make(map[_PDhKey]float64)
+	countTable := make(map[uint64]float64)
 
-  // Fill the frequency table
-  for _, row := range ds {
-    class := row[len(row)-1]
-    countTable[class] += 1
-    for feature, featureValue := range row[:len(row)-1] {
-      freqTable[PDhKey{uint64(feature), featureValue, class}] += 1
-    }
-  }
+	// Fill the frequency table
+	for _, row := range ds {
+		class := row[len(row)-1]
+		countTable[class]++
+		for feature, featureValue := range row[:len(row)-1] {
+			freqTable[_PDhKey{uint64(feature), featureValue, class}]++
+		}
+	}
 
-  // Obtain relative frequencies
-  for key, value := range freqTable {
-    freqTable[key] = value / countTable[key.class]
-  }
+	// Obtain relative frequencies
+	for key, value := range freqTable {
+		freqTable[key] = value / countTable[key.class]
+	}
 
-  nb.PDh = freqTable
+	nb.PDh = freqTable
 }
 
+// NewNaiveBayes creates a new Naive Bayes Classifier
 func NewNaiveBayes(probH []float64) NaiveBayes {
-  nb := NaiveBayes{}
-  nb.Ph = probH
+	nb := NaiveBayes{}
+	nb.Ph = probH
 
-  return nb
+	return nb
 }
---
+++ cmd/britanicos/britanicos.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+
+	"github.com/Giulianos/ml-naive-bayes-classifier/classifier"
+)
+
+var natMapping []string = []string{"I", "E"}
+
+func loadDataSet(path string) [][]uint64 {
+	file, _ := os.Open(path)
+	r := bufio.NewReader(file)
+	csvReader := csv.NewReader(r)
+
+	var ds [][]uint64
+
+	// Discard header
+	csvReader.Read()
+	for {
+		record, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		var row []uint64
+		for _, fieldValue := range record[:len(record)-1] { // Omit nationality
+			temp, _ := strconv.ParseUint(fieldValue, 10, 64)
+			row = append(row, temp)
+		}
+		// Convert nationality to 1/0
+		var convNat uint64
+		if record[len(record)-1] == natMapping[1] {
+			convNat = 1
+		}
+		row = append(row, convNat)
+		ds = append(ds, row)
+	}
+
+	return ds
+}
+
+func main() {
+
+	// Create classifier passing prior class probability
+	nb := classifier.NewNaiveBayes([]float64{0.5, 0.5})
+
+	// Load dataset from file
+	ds := loadDataSet(os.Args[1])
+
+	// Train the classifier
+	nb.Train(ds)
+
+	// Predict the nationality based on the passed preferences
+	class, prioriProb := nb.Predict([]uint64{1, 0, 1, 1, 0})
+
+	fmt.Printf("Las preferencias corresponden a %s (%f)\n", natMapping[class], prioriProb)
+
+}
